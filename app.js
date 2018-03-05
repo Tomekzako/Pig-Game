ff6fb9d5611cdf5fdf06aeba537c5d8965d05fa8
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-03-05 14:47:15 +0100

    Finish game and declare winner

--- app.js
+++ app.js
@@ -38,8 +38,15 @@ document.addEventListener('DOMContentLoaded', function () {
         scores[activePlayer] += roundScore;
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        nextPlayer();
-
+        
+        if (scores[activePlayer] >= 10){
+            document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
+            document.querySelector('.player' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player' + activePlayer + '-panel').classList.remove('active');
+            document.querySelector('.dice').style.display = 'none';
+        } else {
+            nextPlayer();    
+        }
     });
 
 
